0f0e2ffe47cc736ab4f531b56c16ca5910b09d21
Alfred J. Kwak <alfredjkwak@protonmail.com>
2017-02-18 00:22:02 +0200

    add parameters

--- generators/app/templates/server/routes.js
+++ generators/app/templates/server/routes.js
@@ -4,7 +4,16 @@ Api.addRoute('<%= p %>', {}, {<% for (var e in api.paths[p]) { %>
 <%= e %>: {
     swagger: {
       tags: ['<%= api.paths[p][e].tags %>'],
-      description: '<%= api.paths[p][e].description %>',
+      description: '<%= api.paths[p][e].description %>',<% if(api.paths[p][e].parameters) { %>
+      parameters: [<% for (var param in api.paths[p][e].parameters) { %>
+        {
+          name: '<%= api.paths[p][e].parameters[param].name %>',
+          in: '<%= api.paths[p][e].parameters[param].in %>',
+          description: '<%= api.paths[p][e].parameters[param].description %>',
+          required: <%= api.paths[p][e].parameters[param].required %>,
+          type: '<%= api.paths[p][e].parameters[param].type %>',
+        },<% } %>
+      ],<% } %>
       responses: {<% for (var r in api.paths[p][e].responses) { %>
         <%= r %>: {
           description: '<%= api.paths[p][e].responses[r].description %>',

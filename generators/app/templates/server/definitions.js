d8453fc54e046ac1e9cd066bb42f111dffaa0f36
Alfred J. Kwak <alfredjkwak@protonmail.com>
2017-02-18 00:32:22 +0200

    add comment, remove if

--- generators/app/templates/server/definitions.js
+++ generators/app/templates/server/definitions.js
@@ -1,3 +1,3 @@
-import Api from './api'; <% if(api.definitions) { %>
+import Api from './api';
+// Add swagger definitions
 Api.swagger.definitions = <%-JSON.stringify(api.definitions, null, 2)%>
-<% } %>
